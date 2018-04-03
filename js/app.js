d32cf6d28234d3f2e989139f0e23e7902556f561
fmmricardo <ricardofilipe5@sapo.pt>
2018-04-03 13:24:35 +0100

    new javascript Update

--- js/app.js
+++ js/app.js
@@ -1,21 +1,9 @@
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1TAttZmIi2fyO91M8lOy8nYsNRqwX-W7l4hTQywYAapE/edit?usp=sharing';
-
-function init() {
-  Tabletop.init( { key: publicSpreadsheetUrl,
-                   callback: showInfo,
-                   simpleSheet: true } )
-}
-
-function showInfo(data, tabletop) {
-  alert('Successfully processed!')
-  console.log(data);
-
-
-}
-
-window.addEventListener('DOMContentLoaded', init)
-
 jQuery(document).ready(function ($) {
+  var publicSpreadsheetUrl = '1TAttZmIi2fyO91M8lOy8nYsNRqwX-W7l4hTQywYAapE',
+      toolsArr = [],
+      topicsArr = [],
+      skillsArr = [],
+      Button = document.getElementById("buttontool");
 
   //Open Rules
   jQuery(document).on("click",".OpenRules", function() {
@@ -26,46 +14,74 @@ jQuery(document).ready(function ($) {
   jQuery(document).on("click",".Button--Close", function() {
     jQuery('.Overlay').css('display','none');
   });
+
+  // Initialize Google spreadsheet content
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } );
+  }
+
+  // Process the information from the Spreadsheet
+  function showInfo(data) {
+
+    console.log(data);
+
+    // get Tools
+    for (var i = 0; i < data.length; i++) {
+      if ( data[i].tools !== "" ) {
+        toolsArr.push(data[i].tools);
+      }
+    }
+    console.log(toolsArr);
+
+    // get Topics
+    for (var i = 0; i < data.length; i++) {
+      if ( data[i].topics !== "" ) {
+        topicsArr.push(data[i].topics);
+      }
+    }
+    console.log(topicsArr);
+
+    // get Skills
+    for (var i = 0; i < data.length; i++) {
+      if ( data[i].skills !== "" ) {
+        skillsArr.push(data[i].skills);
+      }
+    }
+    console.log(skillsArr);
+
+  }
+
+  // start init
+  window.addEventListener('DOMContentLoaded', init());
+
+  // get max and do random number for Tools
+  function getTools(){
+    let randomTool = toolsArr[Math.floor(Math.random()*toolsArr.length)];
+    return randomTool;
+  };
+
+  function getTheme(){
+    let randomTheme = topicsArr[Math.floor(Math.random()*topicsArr.length)];
+    return randomTheme;
+  };
+
+  function getSkills(){
+    let randomSkills = skillsArr[Math.floor(Math.random()*skillsArr.length)];
+    return randomSkills;
+  };
+
+  Button.onclick = function() {
+    var resultTool = getTools(),
+        resultTheme = getTheme(),
+        resultSkills = getSkills(),
+        changeTool = document.getElementById("placeholdertools"),
+        changeTheme = document.getElementById("placeholdertopics"),
+        changeSkills = document.getElementById("placeholderskills");
+
+    changeTool.innerHTML = resultTool;
+    changeTheme.innerHTML = resultTheme;
+    changeSkills.innerHTML = resultSkills;
+  };
 });
-
-//array with tools
-let tools=[
-    // "Photoshop",
-    // "Illustrator",
-    // "Sketch",
-    // "Marvel",
-    // "Figma",
-    // "Canvas Business Model",
-    // "Git"
-];
-
-let theme=[
-  "a",
-  "b",
-  "c"
-];
-
-
-// choose a random element from the tools array
-function getTools(){
-  let randomTool = tools[Math.floor(Math.random()*tools.length)];
-  return randomTool;
-};
-
-function getTheme(){
-  let randomTheme = theme[Math.floor(Math.random()*theme.length)];
-  return randomTheme;
-};
-
-var Button = document.getElementById("buttontool");
-
-Button.onclick = function() {
-  var resultTool = getTools();
-  var resultTheme = getTheme();
-
-  var placeholder = document.getElementById("placeholdertools");
-  placeholder.innerHTML = resultTool;
-
-  var placeholdertheme = document.getElementById("placeholdertheme");
-  placeholdertheme.innerHTML = resultTheme;
-};
